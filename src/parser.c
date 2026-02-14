abdb0e722eae216a84eb38d969ac47dad0c30873
Pritam Mohanta <pritamohanta.in.gmail.com>
2026-02-14 14:08:01 +0530

    Implement command parsing functions for input and output redirection

--- src/parser.c
+++ src/parser.c
@@ -7,7 +7,55 @@ static void skip_ws(const char *s, size_t *i) {
     while (s[*i] && (s[*i] == ' ' || s[*i] == '\t' || s[*i] == '\n')) { (*i)++; }
 }
 
+static bool word(const char *s, size_t *i) {
+    skip_ws(s, i) ;
+    size_t j = *i ;
 
+    if(!s[j]) return false ;
+
+    for(;s[j] && s[j] != '|' && s[j] != '&' && s[j] != '>' && s[j] != '<' && s[j] != ';'; j++) 
+    
+    if(j == *i) return false ;
+    *i = j ;
+    return true ;
+}
+
+static bool input_redir(const char *s, size_t *i) {
+    size_t save = *i ;
+    skip_ws(s, i) ;
+    if(s[*i] != '<') return false ;
+    (*i)++ ;
+    if(!word(s, &i)) {
+        *i = save ; return false ;
+    }
+    return true ;
+}
+static bool output_redir(const char *s, size_t *i) {
+    size_t save = *i ;
+    skip_ws(s, i) ;
+    if(s[*i] != '>') return false ;
+    (*i)++ ;
+    if(!word(s, &i)) {
+        *i = save ; return false ;
+    }
+    return true ;
+}
+
+static bool atomic(const char *s, size_t *i) {
+    skip_ws(s, &i) ;
+    if(!word(s, &i)) return false ;
+
+    for(;;) {
+        size_t save = *i ;
+        skip_ws(s, &i) ;
+
+        if(input_redir(s, &i) || output_redir(s, &i) || word(s, &i)) {
+            continue ;
+        }
+        *i = save ; break ;
+    }
+    return true ;
+}
 
 static bool valid_cmd_seg(const char *s, size_t *i) {
     skip_ws(s, i) ;
